2ffd1117f79ef6854d842fcaa8f842e12956f094
Riley Smith <rileyadamsmith@gmail.com>
2024-09-06 07:37:30 +0800

    Delete validateInviteCode.js

--- validateInviteCode.js
+++
@@ -1,7 +0,0 @@
-function validateInviteCode(event) {
-    const inviteCode = document.querySelector('input[name="invitecode"]').value;
-    if (inviteCode !== "2208") {
-      alert("That invite code is incorrect. Please try again.");
-      event.preventDefault();
-    }
-  }
