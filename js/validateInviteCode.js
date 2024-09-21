ed06a9eaf52e52f2e7b95f179bbb55ab7379b391
Riley Smith <rileyadamsmith@gmail.com>
2024-09-21 21:50:07 +0800

    // URGENT: Couldn't add additional invite code, breaking the entire function.

--- js/validateInviteCode.js
+++ js/validateInviteCode.js
@@ -4,7 +4,7 @@
 
 function validateInviteCode(event) {
     const inviteCode = document.querySelector('input[name="invitecode"]').value;
-    if (inviteCode !== "2208", "2624") {
+    if (inviteCode !== "2208") {
       alert("That invite code is incorrect. Please try again.");
       event.preventDefault();
     }
