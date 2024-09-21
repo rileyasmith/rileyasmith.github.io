7da5b72b2d5f05f69bb61ee2077039980a730df3
Riley Smith <rileyadamsmith@gmail.com>
2024-09-21 15:14:14 +0800

    One of many quality of life updates.

--- js/validateInviteCode.js
+++ js/validateInviteCode.js
@@ -1,3 +1,7 @@
+// Written by Riley Smith (rileyasmith) ~ 21 September 2024
+// This function allows access to 'homepage.html' when the user enters the right code.
+// In this case, the access code is "2208" and all other integers will be denied.
+
 function validateInviteCode(event) {
     const inviteCode = document.querySelector('input[name="invitecode"]').value;
     if (inviteCode !== "2208") {
