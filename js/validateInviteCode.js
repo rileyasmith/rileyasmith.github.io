fdc609622dbbf0646ae01131aca2658743fbe6f3
Riley Smith <rileyadamsmith@gmail.com>
2024-09-28 22:10:00 +0800

    Changed invite code to a password and fixed source code access link.

--- js/validateInviteCode.js
+++ js/validateInviteCode.js
@@ -1,10 +1,10 @@
 // Written by Riley Smith (rileyasmith) ~ 21 September 2024
 // This function allows access to 'homepage.html' when the user enters the right code.
-// In this case, the access code is "2208" and all other integers will be denied.
+// In this case, the access code is "access" and all other attempts will be denied.
 
 function validateInviteCode(event) {
     const inviteCode = document.querySelector('input[name="invitecode"]').value;
-    if (inviteCode !== "2208") {
+    if (inviteCode !== "access") {
       alert("That invite code is incorrect. Please try again.");
       event.preventDefault();
     }
