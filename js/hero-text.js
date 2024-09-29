1bb6794ed95bc6ae897f2dcf08819171a9c87149
Riley Smith <rileyadamsmith@gmail.com>
2024-09-29 22:12:22 +0800

    Bug fixes.

--- js/hero-text.js
+++ js/hero-text.js
@@ -1,6 +1,6 @@
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-document.querySelector("hero-text").onmouseover = event => {
+document.querySelector("h1").onmouseover = event => {
     event.target.innerText = event.target.innerText.split("")
     .map(letter => letters[Math.floor(Math.random() * 26)])
     .join("");
