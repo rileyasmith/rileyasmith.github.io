c79527414662607996e35488592ad3b5255fb2e9
Riley Smith <rileyadamsmith@gmail.com>
2024-09-30 20:39:08 +0800

    Adding TailwindCSS.

--- js/validateInviteCode.js
+++ js/validateInviteCode.js
@@ -1,7 +1,3 @@
-// Written by Riley Smith (rileyasmith) ~ 21 September 2024
-// This function allows access to 'homepage.html' when the user enters the right code.
-// In this case, the access code is "access" and all other attempts will be denied.
-
 function validateInviteCode(event) {
     const inviteCode = document.querySelector('input[name="invitecode"]').value;
     if (inviteCode !== "access") {
