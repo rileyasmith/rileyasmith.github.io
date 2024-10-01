db4b29c28de414fff24ba55377431641ed719ce4
Riley Smith <rileyadamsmith@gmail.com>
2024-10-01 21:42:09 +0800

    Fixing stuff.

--- js/validateInviteCode.js
+++ js/validateInviteCode.js
@@ -1,6 +1,6 @@
 function validateInviteCode(event) {
     const inviteCode = document.querySelector('input[name="invitecode"]').value;
-    if (inviteCode !== "access") {
+    if (inviteCode !== "betaCA") {
       alert("That invite code is incorrect. Please try again.");
       event.preventDefault();
     }
