0532addc6ae646c7d666e72fdc6061f4ad7b70d3
Riley Smith <rileyadamsmith@gmail.com>
2024-10-01 18:01:10 +0800

    Homepage revamp.

--- js/hero-text.js
+++
@@ -1,7 +0,0 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-
-document.querySelector("h1").onmouseover = event => {
-    event.target.innerText = event.target.innerText.split("")
-    .map(letter => letters[Math.floor(Math.random() * 26)])
-    .join("");
-}
